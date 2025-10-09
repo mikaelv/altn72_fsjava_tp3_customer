16e7b7ad0d877f262647293a67fe48e4c81836d1
Mikael Valot <mikael.valot@efrei.fr>
2025-10-09 22:07:09 +0200

    added swagger

--- src/main/java/org/efrei/altn72/customer/resource/CustomerApplication.java
+++ src/main/java/org/efrei/altn72/customer/resource/CustomerApplication.java
@@ -1,9 +1,19 @@
 package org.efrei.altn72.customer.resource;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import jakarta.ws.rs.ApplicationPath;
 import jakarta.ws.rs.core.Application;
 
 @ApplicationPath("/api")
+@OpenAPIDefinition(
+        info = @Info(
+                title = "Customer API",
+                version = "1.0.0",
+                description = "REST API for managing customers"
+        ),
+        servers = @io.swagger.v3.oas.annotations.servers.Server(url = "/customer")
+)
 public class CustomerApplication extends Application {
 
 }
